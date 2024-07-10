4832566eea60b66efff7518974c83659564533e1
Yessine Ben Hamroun <146778387+Yessine-BenHamroun@users.noreply.github.com>
2024-07-10 10:39:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,5 +49,5 @@ if options:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),user_container_width=True)
+st.text(fruityvice_response.json())
+# fv_df = st.dataframe(data=fruityvice_response.json(),user_container_width=True)
