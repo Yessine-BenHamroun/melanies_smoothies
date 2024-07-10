4c1edec79a564b6da5f021edfa18197e16d52c0e
Yessine Ben Hamroun <146778387+Yessine-BenHamroun@users.noreply.github.com>
2024-07-10 10:46:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,10 @@ if options:
     ingredients_string= ''
 
     for fruit_chosen in options:
-         ingredients_string+=fruit_chosen+ ' '
+        ingredients_string+=fruit_chosen+ ' '
+        st.subheader(fruit_chosen+ ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order,order_filled)
@@ -48,6 +51,4 @@ if options:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
